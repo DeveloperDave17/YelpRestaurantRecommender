acb82dc1f11e477004b9615c289126561a77d5a9
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-02-06 13:19:26 -0500

    Recreate File Structure

--- src/main/java/com/yelp/restaurantFinder/RestaurantFinderApplication.java
+++ src/main/java/com/yelp/restaurantFinder/RestaurantFinderApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestaurantFinderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestaurantFinderApplication.class, args);
+//		SpringApplication.run(RestaurantFinderApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
