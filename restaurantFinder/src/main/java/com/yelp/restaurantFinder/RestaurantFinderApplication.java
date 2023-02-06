88640e0897bdf76a941f278cdf3f3e9ea1f4ecd1
David Hennigan <110121137+DeveloperDave17@users.noreply.github.com>
2023-02-06 09:29:15 -0500

    Initializing Project

---
+++ restaurantFinder/src/main/java/com/yelp/restaurantFinder/RestaurantFinderApplication.java
@@ -0,0 +1,13 @@
+package com.yelp.restaurantFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantFinderApplication.class, args);
+	}
+
+}
---
+++ restaurantFinder/src/test/java/com/yelp/restaurantFinder/RestaurantFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yelp.restaurantFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
