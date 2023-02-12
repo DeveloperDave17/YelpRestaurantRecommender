3c7cc629740e466beee087327f13f6acdf8b79e6
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-02-12 10:12:59 -0500

    Create Review encapsulation and getter methods

--- src/main/java/com/yelp/restaurantFinder/Review.java
+++ src/main/java/com/yelp/restaurantFinder/Review.java
@@ -2,10 +2,10 @@ package com.yelp.restaurantFinder;
 
 public class Review {
 
-    String business_id;
-    int stars;
-    String text;
-    int useful;
+    private String business_id;
+    private int stars;
+    private String text;
+    private int useful;
 
     public Review(String business_id, int stars, String text, int useful){
         this.business_id = business_id;
@@ -13,4 +13,9 @@ public class Review {
         this.text = text;
         this.useful = useful;
     }
+
+    public String getBusiness_id() { return business_id; }
+    public int getStars() { return stars; }
+    public String getText() { return text; }
+    public int getUseful() { return useful; }
 }
