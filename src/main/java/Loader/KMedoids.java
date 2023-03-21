aee5037a5590e39a973614441759a81661df040e
DeveloperDave17 <DavidHennigan17@gmail.com>
2023-03-21 09:33:39 -0400

    Update KMedoids simulation size

--- src/main/java/Loader/KMedoids.java
+++ src/main/java/Loader/KMedoids.java
@@ -111,7 +111,7 @@ public class KMedoids {
 
         HashMap<Business, FreqHT> businessFreqHTHashMap = new HashMap<>();
 
-        for (int i = 0; i < 20; i++){
+        for (int i = 0; i < 100; i++){
             Business barMedoid = pickOne(bars);
 
             Business americanMedoid = pickOne(american);
